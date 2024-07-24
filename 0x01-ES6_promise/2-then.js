71a104aec41500ddbc4196c8770809d6c310f7b5
vale-hash <newlechim@gmail.com>
2024-07-24 22:56:52 +0100

    2 then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,16 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
         promise.then((promise) => {
+
                 console.log('Got a response from the API');
+                return {
+                        status: 200,
+                        body: 'success',
+                };
         })
         promise.catch((promise) => {
                 console.log("GOt a response from the API")
+                return new Error();
         })
 }
+
+export default handleResponseFromAPI;
