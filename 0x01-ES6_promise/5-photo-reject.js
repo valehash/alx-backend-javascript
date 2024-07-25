cc54853de266de489b540aa4698088102f771ab4
vale-hash <newlechim@gmail.com>
2024-07-25 10:31:36 +0100

    5-rejected

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadphoto(fileName) {
     return new Promise((respolve, reject) => { 
         const response = ` ${fileName} cannot be processed`
-        reject(new Error(response));
+        reject(Error(response));
     })
 }
