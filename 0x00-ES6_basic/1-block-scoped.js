00b7876ae77aca38073484cf9caeb32a2e59a044
vale-hash <newlechim@gmail.com>
2024-07-26 13:57:51 +0100

    const everywhere

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
-    let task2 = true;
+    const task2 = true;
   
     if (trueOrFalse) {
         const task = true;
-        let task2 = false;
+        const task2 = false;
     }
   
     return [task, task2];
