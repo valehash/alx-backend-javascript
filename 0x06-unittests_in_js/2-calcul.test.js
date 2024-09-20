5b034759ee56e06c25f068bcb40c4f1b47b5d541
vale-hash <newlechim@gmail.com>
2024-09-20 20:48:02 +0100

    task 2

---
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -0,0 +1,11 @@
+const expect = require("chai").expect;
+const calculator = require("./2-calcul.js")
+
+describe('calculation', () => {
+  it("should add two rounded numbers and give the correct output", () => {
+    expect(calculator('SUM', 2.1, 3.1)).to.equal(5);
+    expect(calculator('SUBTRACT',3.7, 9)).to.equal(5);
+    expect(calculator("DIVIDE",8, 4.4)).to.equal(2);
+    expect(calculator("DIVIDE",8, 0.2)).to.equal("Error");
+  });
+});
