5759fcb286f53b99862933b772600be4308e3807
valehash <newlechim@gmail.com>
2024-10-20 12:22:36 +0100

    task 1-stdin linted

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`your name is: ${name}`);
-
-  console.log('This important software is now closing');
   process.exit();
+  
+});
+
+process.on('exit', () => {
+	console.log('This important software is now closing');
 });
