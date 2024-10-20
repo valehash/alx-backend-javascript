ed93ff1039433adeab0da3564ed707e974a84788
valehash <newlechim@gmail.com>
2024-10-20 12:35:39 +0100

    changed console.log to process write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`your name is: ${name}`);
 
   if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
   }
 
   process.exit();
