a087bb314b3d488c18a11f0bbf5e3bdd0e8c1001
valehash <newlechim@gmail.com>
2024-10-20 11:27:51 +0100

    linted

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 function displayMessage(message) {
-	//process.stdout.write(message);
-	console.log(message);
+  // process.stdout.write(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
