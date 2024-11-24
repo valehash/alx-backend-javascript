c6865ffac29a2140c6e01e9ca373b9f69a0275cc
vale_hash <elekwachi50@gmail.com>
2024-11-24 20:14:56 +0100

    spread syntax

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './hoisttest.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
